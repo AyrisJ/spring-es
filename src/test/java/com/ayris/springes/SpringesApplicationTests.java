debca78de6550ef9d155a88d0a0a2414dce6e51c
ayris <nleisure@163.com>
2020-11-11 00:09:20 +0800

    startup first

---
+++ src/test/java/com/ayris/springes/SpringesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayris.springes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
